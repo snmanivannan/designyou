bc7fd8f60099b618b06e3d824d24fe4f0987b282
snmanivannan <snmanivannan>
2025-04-12 11:34:46 +0530

    correct typo

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import Navbar from './Navbar'
-import MobileMenu from './Mobilemenu'
+import MobileMenu from './MobileMenu'
 
 export default function Header() {
   const [scrolled, setScrolled] = useState(false)
